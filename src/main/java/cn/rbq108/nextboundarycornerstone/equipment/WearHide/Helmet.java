64a05b7e1e486b9ef15fe71ef3c867a574a7f52d
Administrator <2903253802@qq.com>
2026-08-23 02:38:12 +0800

    头盔加入，之前遗留屎山修改（拦截头盔之类的）

--- src/main/java/cn/rbq108/nextboundarycornerstone/equipment/WearHide/Helmet.java
+++ src/main/java/cn/rbq108/nextboundarycornerstone/equipment/WearHide/Helmet.java
@@ -14,47 +14,47 @@ import java.util.WeakHashMap;
 @EventBusSubscriber(modid = main.MODID, value = Dist.CLIENT)
 public class Helmet {
 
-    // 临时存放被摘下来的头盔（支持多人联机，一人一个格子）
+    // 涓存椂瀛樻斁琚憳涓嬫潵鐨勫ご鐩旓紙鏀寔澶氫汉鑱旀満锛屼竴浜轰竴涓牸瀛愶級
     private static final WeakHashMap<Player, ItemStack> hiddenHelmets = new WeakHashMap<>();
 
-    // 渲染前没收头盔
+    // 娓叉煋鍓嶆病鏀跺ご鐩?
     @SubscribeEvent
     public static void onRenderPlayerPre(RenderPlayerEvent.Pre event) {
         Player player = event.getEntity();
 
         if (GlobalVariables.B_LowGravity) {
-            //绕过底层限制，直接去玩家的背包内存列表里拿东西(索引3对应头盔栏)
+            //缁曡繃搴曞眰闄愬埗锛岀洿鎺ュ幓鐜╁鐨勮儗鍖呭唴瀛樺垪琛ㄩ噷鎷夸笢瑗?绱㈠紩3瀵瑰簲澶寸洈鏍?
             ItemStack headItem = player.getInventory().armor.get(3);
 
             if (!headItem.isEmpty()) {
                 String itemName = headItem.getItem().toString();
 
-                // 判定头盔是否属于模组自己的东西，否则不渲染（毕竟为了适配现在这个头部运动，要专门写对应的旋转）
-                if (!itemName.contains("test:pilot_helmet")) {
+                // 鍒ゅ畾澶寸洈鏄惁灞炰簬妯＄粍鑷繁鐨勪笢瑗匡紝鍚﹀垯涓嶆覆鏌擄紙姣曠珶涓轰簡閫傞厤鐜板湪杩欎釜澶撮儴杩愬姩锛岃涓撻棬鍐欏搴旂殑鏃嬭浆锛?
+                if (!itemName.contains("test:pilot_helmet") && !itemName.contains("extravehicular_spacesuit_helmet")) {
 
-                    //找个犄角旮旯塞着
+                    //鎵句釜鐘勮鏃棷濉炵潃
                     hiddenHelmets.put(player, headItem);
 
-                    // 清空背包里的头盔
-                    // 绝不能使用setItemSlot（不信的话自己试试），避开了游戏底层的穿脱音效和护甲重算（后注：真的避免了吗）
+                    // 娓呯┖鑳屽寘閲岀殑澶寸洈
+                    // 缁濅笉鑳戒娇鐢╯etItemSlot锛堜笉淇＄殑璇濊嚜宸辫瘯璇曪級锛岄伩寮€浜嗘父鎴忓簳灞傜殑绌胯劚闊虫晥鍜屾姢鐢查噸绠楋紙鍚庢敞锛氱湡鐨勯伩鍏嶄簡鍚楋級
                     player.getInventory().armor.set(3, ItemStack.EMPTY);
                 }
             }
         }
     }
 
-    //  Post后悄悄塞回去
+    //  Post鍚庢倓鎮勫鍥炲幓
     @SubscribeEvent
     public static void onRenderPlayerPost(RenderPlayerEvent.Post event) {
         Player player = event.getEntity();
 
-        // 检查储物柜里有没有扣押他的头盔
+        // 妫€鏌ュ偍鐗╂煖閲屾湁娌℃湁鎵ｆ娂浠栫殑澶寸洈
         if (hiddenHelmets.containsKey(player)) {
 
-            // 悄悄把头盔塞回玩家的包里，假装什么都没发生过骂我！
+            // 鎮勬倓鎶婂ご鐩斿鍥炵帺瀹剁殑鍖呴噷锛屽亣瑁呬粈涔堥兘娌″彂鐢熻繃楠傛垜锛?
             player.getInventory().armor.set(3, hiddenHelmets.get(player));
 
-            // 清理储物柜
+            // 娓呯悊鍌ㄧ墿鏌?
             hiddenHelmets.remove(player);
         }
     }
